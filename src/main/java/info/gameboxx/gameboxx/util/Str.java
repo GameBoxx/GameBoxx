88a8df20359425b889673a95b1e81d21c09e8e0e
Jos <josroossien@hotmail.com>
2016-02-23 07:05:16 +0100

    Added capitalize string utility method.

--- src/main/java/info/gameboxx/gameboxx/util/Str.java
+++ src/main/java/info/gameboxx/gameboxx/util/Str.java
@@ -164,6 +164,18 @@ public class Str {
         return sections;
     }
 
+    /**
+     * Capitalize the first character of a string.
+     * @param str
+     * @return Capitalized string
+     */
+    public static String capitalize(String str) {
+        if (str.length() == 0) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
     public static String wrapStringExact(String string, int length) {
         return string.replaceAll("(.{" + length + "})", "$1\n");
     }
