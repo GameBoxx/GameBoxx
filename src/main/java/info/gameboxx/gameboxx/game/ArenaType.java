08d7b2d7079d851dbe8623c236e7b623334ca9c4
Jos <josroossien@hotmail.com>
2016-02-25 03:19:24 +0100

    Added a list of aliases for arena type enum.

--- src/main/java/info/gameboxx/gameboxx/game/ArenaType.java
+++ src/main/java/info/gameboxx/gameboxx/game/ArenaType.java
@@ -26,7 +26,11 @@
 package info.gameboxx.gameboxx.game;
 
 import com.google.common.collect.Maps;
+import info.gameboxx.gameboxx.util.Str;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,14 +42,14 @@ public enum ArenaType {
      * Each time a new {@link GameSession} is created it will create a copy of the linked template world.
      * This arena type can have multiple {@link GameSession} sessions.
      */
-    WORLD("World"),
+    WORLD("world", "w"),
 
     /**
      * This arena type doesn't have an arena/world attached to it.
      * Basically when a new {@link GameSession} is created it will generate a new world with configurable options.
      * This can be used for games like UHC and such that use a random generated vanilla world.
      */
-    GENERATE_WORLD("Generate world"),
+    GENERATE_WORLD("generateworld", "genworld", "generatew", "genw", "gw", "gworld"),
 
     /**
      * This arena type is the default and it won't have an world or anything attached.
@@ -54,38 +58,55 @@ public enum ArenaType {
      * This should used for smaller games with multiple games per world and such.
      * Just remember that there can only be one session per arena with this type.
      */
-    DEFAULT("Default");
+    DEFAULT("default", "def", "d");
 
-    private String name;
-    private final static Map<String, ArenaType> BY_NAME = Maps.newHashMap();
+    private List<String> aliases;
+    private final static Map<String, ArenaType> MATCH = Maps.newHashMap();
 
     static {
         for (ArenaType type : values()) {
-            BY_NAME.put(type.getName().toLowerCase().replace(" ", ""), type);
-            BY_NAME.put(type.toString().toLowerCase(), type);
+            MATCH.put(type.toString().toLowerCase(), type);
+            MATCH.put(type.toString().toLowerCase().replace(" ", ""), type);
+            for (String alias : type.getAliases()) {
+                MATCH.put(alias.toLowerCase(), type);
+            }
         }
     }
 
-    ArenaType(String name) {
-        this.name = name;
+    ArenaType(String... aliases) {
+        this.aliases = Arrays.asList(aliases);
     }
 
     /**
-     * Get the name of the arena type.
-     * @return Name of the type.
+     * Get a list with aliases for the arena type.
+     * @return List with aliases.
      */
-    public String getName() {
-        return name;
+    public List<String> getAliases() {
+        return aliases;
     }
 
     /**
-     * Get an {@link ArenaType} by name.
-     * The name can be like GENERATE_WORLD or generate world.
-     * @param name The name to get the type for.
-     * @return The {@link ArenaType} from the specified name or {@code null} when there is no type by the specified name.
+     * Get an {@link ArenaType} from the specified name.
+     * The name can be like GENERATE_WORLD or generateworld but it can also be an alias.
+     * @param name The name/alias to get the type for.
+     * @return The {@link ArenaType} from the specified name/alias or {@code null} when there is no type by the specified name.
      */
     public static ArenaType fromName(String name) {
-        return BY_NAME.get(name.toLowerCase().replace(" ", ""));
+        return MATCH.get(name.toLowerCase().replace(" ", ""));
+    }
+
+    /**
+     * Get a map with aliases.
+     * The key is the {@link ArenaType} as string and the value is a list of aliases.
+     * Can be used for {@link info.gameboxx.gameboxx.util.Utils#getAliasesString(String, Map)}
+     * @return Map with names and aliases.
+     */
+    public static Map<String, List<String>> getAliasesMap() {
+        Map<String, List<String>> map = new HashMap<>();
+        for (ArenaType arenaType : values()) {
+            map.put(Str.camelCase(arenaType.toString()), arenaType.getAliases());
+        }
+        return map;
     }
 
 }
