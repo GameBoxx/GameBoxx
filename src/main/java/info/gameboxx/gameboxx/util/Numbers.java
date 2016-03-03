30a867d9ef434cac070a11271b3a7098cd998816
Web2201 <mislav.milicevic22@gmail.com>
2016-03-03 14:41:22 +0100

    Session joining is now based on the largest amount of players.

--- src/main/java/info/gameboxx/gameboxx/util/Numbers.java
+++ src/main/java/info/gameboxx/gameboxx/util/Numbers.java
@@ -33,7 +33,9 @@ import java.util.List;
 public class Numbers {
 
     /**
-     * Returns the smallest entry in the array. If there are multiple entries that are equal, it will choose a random one if 'randomIfMultiple' is true. Otherwise, the first element in the array will be chosen.
+     * Returns the smallest entry in the array.
+     * If there are multiple entries that are equal, it will choose a random one if 'randomIfMultiple' is true.
+     * Otherwise, the first element in the array will be chosen.
      *
      * @param ints {@link Integer} array
      * @return smallest entry in array
@@ -61,7 +63,9 @@ public class Numbers {
     }
 
     /**
-     * Returns the largest entry in the array. If there are multiple entries that are equal, it will choose a random one if 'randomIfMultiple' is true. Otherwise, the first element in the array will be chosen.
+     * Returns the largest entry in the array.
+     * If there are multiple entries that are equal, it will choose a random one if 'randomIfMultiple' is true.
+     * Otherwise, the first element in the array will be chosen.
      *
      * @param ints {@link Integer} array
      * @return largest entry in array
