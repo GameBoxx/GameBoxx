40d98f1ec15a2d94762f9be5d8b26da202486846
Jos <josroossien@hotmail.com>
2016-03-06 11:26:43 +0100

    Added map variant for all the options made so far.

---
+++ src/main/java/info/gameboxx/gameboxx/options/map/BlockMapOption.java
@@ -0,0 +1,62 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2016 GameBoxx <http://gameboxx.info>
+ Copyright (c) 2016 contributors
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package info.gameboxx.gameboxx.options.map;
+
+import info.gameboxx.gameboxx.options.MapOption;
+import info.gameboxx.gameboxx.options.single.BlockOption;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+import java.util.*;
+
+public class BlockMapOption extends MapOption<Block, BlockMapOption, BlockOption> {
+
+    public Map<String, Block> getValues(World world) {
+        Map<String, Block> values = new HashMap<>();
+        for (String key : this.values.keySet()) {
+            values.put(key, getValue(key, world));
+        }
+        return values;
+    }
+
+    public Block getValue(String key, World world) {
+        Block b = getValue(key);
+        if (b == null || world == null) {
+            return b;
+        }
+        return world.getBlockAt(b.getLocation());
+    }
+
+    @Override
+    public BlockOption getSingleOption() {
+        return new BlockOption();
+    }
+
+    @Override
+    public BlockMapOption clone() {
+        return new BlockMapOption();
+    }
+}
---
+++ src/main/java/info/gameboxx/gameboxx/options/map/IntMapOption.java
@@ -0,0 +1,57 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2016 GameBoxx <http://gameboxx.info>
+ Copyright (c) 2016 contributors
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package info.gameboxx.gameboxx.options.map;
+
+import info.gameboxx.gameboxx.options.MapOption;
+import info.gameboxx.gameboxx.options.single.IntOption;
+
+public class IntMapOption extends MapOption<Integer, IntMapOption, IntOption> {
+
+    private Integer min = null;
+    private Integer max = null;
+
+
+    public IntMapOption min(Integer min) {
+        this.min = min;
+        return this;
+    }
+
+    public IntMapOption max(Integer max) {
+        this.max = max;
+        return this;
+    }
+
+
+    @Override
+    public IntOption getSingleOption() {
+        return new IntOption().min(min).max(max);
+    }
+
+    @Override
+    public IntMapOption clone() {
+        return new IntMapOption().min(min).max(max);
+    }
+}
