abf7e23b3c93721d577a56cb35e98f7d498c26e2
Jos <josroossien@hotmail.com>
2016-03-11 16:19:21 +0100

    Fixed several issues with item wrapper. Gonna clean this class up later and add missing methods.

--- src/main/java/info/gameboxx/gameboxx/util/item/EItem.java
+++ src/main/java/info/gameboxx/gameboxx/util/item/EItem.java
@@ -25,6 +25,8 @@
 
 package info.gameboxx.gameboxx.util.item;
 
+import info.gameboxx.gameboxx.GameBoxx;
+import info.gameboxx.gameboxx.util.Enchant;
 import info.gameboxx.gameboxx.util.Parse;
 import info.gameboxx.gameboxx.util.Str;
 import org.bukkit.*;
@@ -75,12 +77,16 @@ public class EItem extends ItemStack {
         return (EItem)super.clone();
     }
 
+    public ItemMeta getMeta() {
+        return getItemMeta() == null ? GameBoxx.get().getServer().getItemFactory().getItemMeta(getType()) : getItemMeta();
+    }
+
     /**
      * Add all ItemFlags to the item.
      * It will hide all tooltips like enchants, attributes, effects etc.
      */
     public EItem addAllFlags(Boolean add) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         for (ItemFlag itemFlag : ItemFlag.values()) {
             if (add) {
                 if (!meta.hasItemFlag(itemFlag)) {
@@ -98,7 +104,7 @@ public class EItem extends ItemStack {
 
     /** Add a specific ItemFlag to the item to hide a certain tooltip. */
     public EItem addFlags(ItemFlag... flags) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         for (ItemFlag itemFlag : flags) {
             if (!meta.hasItemFlag(itemFlag)) {
                 meta.addItemFlags(itemFlag);
@@ -109,32 +115,17 @@ public class EItem extends ItemStack {
     }
 
     /** Add the enchantment glow effect on the item. (Doesn't work for all items like skulls for example not) */
-    public EItem makeGlowing(Boolean glow) {
-        //TODO: Check for protocol lib and if enabled use protocol lib.
-        //TODO: Use custom enchantment.
-        ItemMeta meta = getItemMeta();
+    public EItem setGlowing(Boolean glow) {
         if (glow) {
-            if (!meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)) {
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            }
-            setItemMeta(meta);
-            if (getType() == Material.FISHING_ROD) {
-                addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 0);
-            } else {
-                addUnsafeEnchantment(Enchantment.LURE, 0);
-            }
+            addEnchant(GlowEnchant.get(), 1);
         } else {
-            if (meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)) {
-                meta.removeItemFlags(ItemFlag.HIDE_ENCHANTS);
-            }
-            setItemMeta(meta);
-            if (getType() == Material.FISHING_ROD) {
-                removeEnchantment(Enchantment.ARROW_INFINITE);
-            } else {
-                removeEnchantment(Enchantment.LURE);
-            }
+            removeEnchantment(GlowEnchant.get());
         }
         return this;
+    }
+
+    public boolean isGlowing() {
+        return hasEnchant(GlowEnchant.get());
     }
 
 
@@ -149,7 +140,7 @@ public class EItem extends ItemStack {
 
     /** Set the display name of the item. If color is set to true it will automatically format colors. */
     public EItem setName(String name, Boolean color) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         name = name.replaceAll("(?<!_)_(?!_)", " ");
         meta.setDisplayName(color ? Str.color(name) : name);
         setItemMeta(meta);
@@ -158,10 +149,10 @@ public class EItem extends ItemStack {
 
     /** Get the display name of the item. */
     public String getName() {
-        if (!getItemMeta().hasDisplayName()) {
+        if (!getMeta().hasDisplayName()) {
             return getType().toString().toLowerCase().replace("_", " ");
         }
-        return getItemMeta().getDisplayName();
+        return getMeta().getDisplayName();
     }
 
 
@@ -206,7 +197,7 @@ public class EItem extends ItemStack {
             }
             lore.set(i, loreStr);
         }
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         meta.setLore(lore);
         setItemMeta(meta);
         return this;
@@ -241,7 +232,7 @@ public class EItem extends ItemStack {
             }
             lore.set(i, loreStr);
         }
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta.hasLore()) {
             List<String> prevLore = meta.getLore();
             prevLore.addAll(lore);
@@ -275,7 +266,7 @@ public class EItem extends ItemStack {
         if (color) {
             lore = Str.color(lore);
         }
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
 
         List<String> loreList = meta.hasLore() ? meta.getLore() : new ArrayList<String>();
         Integer lines = Math.max(lineNr + 1, loreList.size());
@@ -296,7 +287,7 @@ public class EItem extends ItemStack {
 
     //Remove lore
     public EItem clearLore() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         meta.setLore(new ArrayList<String>());
         setItemMeta(meta);
         return this;
@@ -304,7 +295,7 @@ public class EItem extends ItemStack {
 
     /** Clear a specific line of lore from the lines. */
     public EItem clearLore(Integer lineNr) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         List<String> lore = meta.getLore();
         if (lore.size() > lineNr) {
             lore.remove(lineNr);
@@ -319,18 +310,18 @@ public class EItem extends ItemStack {
 
     /** Get a list of all the lore lines. */
     public List<String> getLore() {
-        if (!getItemMeta().hasLore()) {
+        if (!getMeta().hasLore()) {
             return new ArrayList<String>();
         }
-        return getItemMeta().getLore();
+        return getMeta().getLore();
     }
 
     /** Get a specific line of lore. */
     public String getLore(Integer lineNr) {
-        if (!getItemMeta().hasLore() || getItemMeta().getLore().size() <= lineNr) {
+        if (!getMeta().hasLore() || getMeta().getLore().size() <= lineNr) {
             return "";
         }
-        return getItemMeta().getLore().get(lineNr);
+        return getMeta().getLore().get(lineNr);
     }
 
     /**
@@ -356,6 +347,27 @@ public class EItem extends ItemStack {
     // ##################################################
     // ################## ENCHANTMENTS ##################
     // ##################################################
+
+    public boolean hasEnchant(Enchantment enchant) {
+        return getMeta().hasEnchant(enchant);
+    }
+
+    public boolean hasEnchants() {
+        return getMeta().hasEnchants();
+    }
+
+    /** Add the given enchantment to the item. */
+    public EItem addEnchant(Enchant enchant) {
+        return addEnchant(enchant.getType(), enchant.getLevel(), true);
+    }
+
+    public List<Enchant> getEnchants() {
+        List<Enchant> enchants = new ArrayList<>();
+        for (Map.Entry<Enchantment, Integer> entry : getEnchantments().entrySet()) {
+            enchants.add(new Enchant(entry.getKey(), entry.getValue()));
+        }
+        return enchants;
+    }
 
     /** Add the given enchantment and level to the item. */
     public EItem addEnchant(Enchantment enchantment, Integer level) {
@@ -399,7 +411,7 @@ public class EItem extends ItemStack {
     // ##################################################
 
     public EItem setMainEffect(PotionEffectType effect) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof PotionMeta) {
             ((PotionMeta)meta).setMainEffect(effect);
             setItemMeta(meta);
@@ -408,7 +420,7 @@ public class EItem extends ItemStack {
     }
 
     public EItem addEffect(PotionEffect effect, Boolean overwrite) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof PotionMeta) {
             ((PotionMeta)meta).addCustomEffect(effect, overwrite);
             setItemMeta(meta);
@@ -426,7 +438,7 @@ public class EItem extends ItemStack {
     }
 
     public EItem setColor(Color color) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof LeatherArmorMeta) {
             ((LeatherArmorMeta)meta).setColor(color);
             setItemMeta(meta);
@@ -435,7 +447,7 @@ public class EItem extends ItemStack {
     }
 
     public Color getColor() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof LeatherArmorMeta) {
             return ((LeatherArmorMeta)meta).getColor();
         }
@@ -449,7 +461,7 @@ public class EItem extends ItemStack {
 
     /** Set the author of the book */
     public EItem setAuthor(String author) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             ((BookMeta)meta).setAuthor(author);
             setItemMeta(meta);
@@ -459,7 +471,7 @@ public class EItem extends ItemStack {
 
     /** Get the author of the book */
     public String getAuthor() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             return ((BookMeta)meta).getAuthor();
         }
@@ -468,7 +480,7 @@ public class EItem extends ItemStack {
 
     /** Set the title of the book */
     public EItem setTitle(String title) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             ((BookMeta)meta).setTitle(title);
             setItemMeta(meta);
@@ -478,7 +490,7 @@ public class EItem extends ItemStack {
 
     /** Get the title of the book */
     public String getTitle() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             return ((BookMeta)meta).getTitle();
         }
@@ -503,7 +515,7 @@ public class EItem extends ItemStack {
      * for several book formatting options. //TODO: Put link for book formatting class.
      */
     public EItem addPage(String... pages) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             ((BookMeta)meta).addPage(pages);
             setItemMeta(meta);
@@ -517,7 +529,7 @@ public class EItem extends ItemStack {
      * for several book formatting options. //TODO: Put link for book formatting class.
      */
     public EItem setPage(Integer page, String content) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             ((BookMeta)meta).setPage(page, content);
             setItemMeta(meta);
@@ -542,7 +554,7 @@ public class EItem extends ItemStack {
      * for several book formatting options. //TODO: Put link for book formatting class.
      */
     public EItem setPages(List<String> pages) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             ((BookMeta)meta).setPages(pages);
             setItemMeta(meta);
@@ -552,7 +564,7 @@ public class EItem extends ItemStack {
 
     /** Get the content of the given page */
     public String getPage(Integer page) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             return ((BookMeta)meta).getPage(page);
         }
@@ -561,7 +573,7 @@ public class EItem extends ItemStack {
 
     /** Get all the pages of the book that have content. */
     public List<String> getPages() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BookMeta) {
             return ((BookMeta)meta).getPages();
         }
@@ -575,7 +587,7 @@ public class EItem extends ItemStack {
 
     /** Set the base color of a banner */
     public EItem setBaseColor(DyeColor color) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             ((BannerMeta)meta).setBaseColor(color);
             setItemMeta(meta);
@@ -585,7 +597,7 @@ public class EItem extends ItemStack {
 
     /** Get the base color of a banner */
     public DyeColor getBaseColor() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             return ((BannerMeta)meta).getBaseColor();
         }
@@ -599,7 +611,7 @@ public class EItem extends ItemStack {
 
     /** Add a pattern to the banner */
     public EItem addPattern(Pattern pattern) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             ((BannerMeta)meta).addPattern(pattern);
             setItemMeta(meta);
@@ -614,7 +626,7 @@ public class EItem extends ItemStack {
 
     /** Insert a pattern to the banner at the given index. */
     public EItem insertPattern(Integer index, Pattern pattern) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             if (((BannerMeta)meta).getPatterns().size() > index) {
                 ((BannerMeta)meta).setPattern(index, pattern);
@@ -633,7 +645,7 @@ public class EItem extends ItemStack {
 
     /** Set the banner patterns to the given array of patterns. */
     public EItem setPatterns(List<Pattern> patterns) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             ((BannerMeta)meta).setPatterns(patterns);
             setItemMeta(meta);
@@ -643,7 +655,7 @@ public class EItem extends ItemStack {
 
     /** Get the banner pattern at the given index. */
     public Pattern getPattern(Integer index) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             return ((BannerMeta)meta).getPattern(index);
         }
@@ -652,7 +664,7 @@ public class EItem extends ItemStack {
 
     /** Get a list with all patterns on the banner */
     public List<Pattern> getPatterns() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof BannerMeta) {
             return ((BannerMeta)meta).getPatterns();
         }
@@ -667,7 +679,7 @@ public class EItem extends ItemStack {
     /** Set the skull owner of the skull. */
     public EItem setSkull(String name) {
         setDurability((short)3);
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof SkullMeta) {
             ((SkullMeta)meta).setOwner(name);
             setItemMeta(meta);
@@ -677,7 +689,7 @@ public class EItem extends ItemStack {
 
     /** Get the skull owner of the skull. */
     public String getSkull() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof SkullMeta) {
             return ((SkullMeta)meta).getOwner();
         }
@@ -691,7 +703,7 @@ public class EItem extends ItemStack {
 
     /** Set the firework rocket power */
     public EItem setPower(Integer power) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             ((FireworkMeta)meta).setPower(power);
             setItemMeta(meta);
@@ -701,7 +713,7 @@ public class EItem extends ItemStack {
 
     /** Get the firework rocket power. */
     public Integer getPower() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             return ((FireworkMeta)meta).getPower();
         }
@@ -710,7 +722,7 @@ public class EItem extends ItemStack {
 
     /** Add a base firework effect. (White ball) */
     public EItem addBaseEffect() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             FireworkEffect.Builder builder = FireworkEffect.builder();
             builder.with(FireworkEffect.Type.BALL).withColor(Color.WHITE);
@@ -722,7 +734,7 @@ public class EItem extends ItemStack {
 
     /** Add a firework effect to the item. */
     public EItem addEffect(FireworkEffect effect) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             ((FireworkMeta)meta).addEffect(effect);
             setItemMeta(meta);
@@ -732,7 +744,7 @@ public class EItem extends ItemStack {
 
     /** Add a firework effect to the item. */
     public EItem addEffect(FireworkEffect.Type type, List<Color> colors, List<Color> fadeColors, Boolean flicker, Boolean trail) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             FireworkEffect.Builder builder = FireworkEffect.builder();
             builder.with(type).withColor(colors).withFade(fadeColors);
@@ -750,7 +762,7 @@ public class EItem extends ItemStack {
 
     /** Get a firework effect at the given index. */
     public FireworkEffect getEffect(Integer index) {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             List<FireworkEffect> effects = ((FireworkMeta)meta).getEffects();
             if (effects.size() > index) {
@@ -762,7 +774,7 @@ public class EItem extends ItemStack {
 
     /** Get a list of all the firework effects on the item. */
     public List<FireworkEffect> getEffects() {
-        ItemMeta meta = getItemMeta();
+        ItemMeta meta = getMeta();
         if (meta instanceof FireworkMeta) {
             return ((FireworkMeta)meta).getEffects();
         }
